b3b3b34c64b6f88d13b9579e323931c29b725b36
nsmolyar <nsmolyar@tradingview.com>
2025-05-07 20:20:31 +0400

    Changes to registration page

--- opencart_docker/tests/pages/registration_page.py
+++ opencart_docker/tests/pages/registration_page.py
@@ -7,12 +7,13 @@ from opencart_docker.tests.pages.base_page import BaseComponent
 
 
 class RegistrationPage(BaseComponent):
+    DEFAULT_REGISTRATION_URL = 'http://localhost/index.php?route=account/register&language=en-gb'
     def __init__(
             self,
             context: 'BrowserContext',
             page: 'Page',
             rel_url: str = '',
-            full_url: Optional[str] = None,
+            full_url: str = DEFAULT_REGISTRATION_URL,
             url_parameters: Optional[str] = None,
             request: Optional['pytest.FixtureRequest'] = None
     ):
--- opencart_docker/tests/registration_and_login.py
+++ opencart_docker/tests/registration_and_login.py
@@ -1,6 +1,6 @@
 import allure
 import pytest
-import uuid
+from faker import Faker
 from playwright.sync_api import Page
 from opencart_docker.tests.pages.registration_page import RegistrationPage
 
@@ -9,11 +9,15 @@ from opencart_docker.tests.pages.registration_page import RegistrationPage
 @allure.story('Register new user with valid credentials')
 @allure.severity(allure.severity_level.NORMAL)
 @pytest.mark.registration
-def test_user_registration(page: Page, clean_test_users):
+def test_user_registration_via_main(page: Page, clean_test_users):
     """Test successful user registration with valid credentials."""
-    unique_id = uuid.uuid4().hex[:8]
-    test_email = f'testuser_{unique_id}@example.com'
-    test_password = 'SecurePass123!'
+    fake = Faker()
+    user_data = {
+        'name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'email': fake.email(),
+        'password': fake.password(length=8, special_chars=True),
+    }
 
     registration_page = RegistrationPage(
         context=page.context,
@@ -25,25 +29,34 @@ def test_user_registration(page: Page, clean_test_users):
         page.goto('http://localhost/index.php?route=account/logout')
         page.click(registration_page.continue_button)
 
-    with allure.step('2. Navigate to registration page'):
+    with allure.step('2. Register new user'):
         registration_page.navigate()
+        registration_page.fill_form(**user_data)
 
-    with allure.step('3. Fill registration form with valid details'):
-        registration_page.fill_form(
-            name=f'Test User {unique_id}',
-            last_name=f'Test Lastname {unique_id}',
-            email=test_email,
-            password=test_password,
-        )
-
-    with allure.step('4. Accept privacy policy and submit the form'):
+    with allure.step('3. Accept privacy policy and submit the form'):
         registration_page.accept_privacy_policy()
         registration_page.submit_form()
 
-    with allure.step('5. Verify successful registration'):
+    with allure.step('4. Verify successful registration'):
         assert "route=product/search" in page.url
         allure.attach(
             page.screenshot(full_page=True),
             name="post-registration",
             attachment_type=allure.attachment_type.PNG
         )
+
+
+# @allure.feature('User registration')
+# @allure.story('Register new user with valid credentials')
+# @allure.severity(allure.severity_level.NORMAL)
+# @pytest.mark.registration
+# def test_user_registration_via_login_page(page: Page, clean_test_users):
+#     """Test successful user registration with valid credentials on login page"""
+#     unique_id = uuid.uuid4().hex[:8]
+#     test_email = f'testuser_{unique_id}@example.com'
+#     test_password = 'SecurePass123!'
+#
+# #
+# #
+
+
