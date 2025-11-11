75baea16da6ea00d47b7fc0966b8e84f7c5b8b18
nsmolyar <nsmolyar@tradingview.com>
2025-11-11 17:07:23 +0400

    refactored MainPage class

--- tests/pages/main_page.py
+++ tests/pages/main_page.py
@@ -1,24 +1,20 @@
 from typing import Optional
 
 import pytest
-from playwright.sync_api import BrowserContext, Page
+from playwright.sync_api import BrowserContext, Page, Locator
 
-from tests.pages.base_page import BaseComponent
+from config.urls import URLs
+from tests.pages.base_component import BaseComponent
 
 class MainPage(BaseComponent):
     def __init__(
             self,
-            context: 'BrowserContext',
-            page: 'Page',
-            rel_url: str = '',
-            full_url: str = '',
-            url_parameters: Optional[str] = None,
-            request: Optional['pytest.FixtureRequest'] = None
+            context: BrowserContext,
+            page: Page,
+            request: Optional[pytest.FixtureRequest] = None
     ):
         super().__init__(context, page, request)
-        self.rel_url = rel_url
-        self._full_url = full_url or ""
-        self._url_parameters = url_parameters
+        self.url = URLs.BASE_URL
 
         self.currency_dropdown = page.locator("#form-currency")
         self.currency_dropdown_menu = page.locator("#form-currency .dropdown-menu.show")
@@ -33,15 +29,28 @@ class MainPage(BaseComponent):
         self.success_alert = page.locator("[class*='alert-success']")
         self.item_cart_dropdown_menu = page.locator("[class*='dropdown-menu'] [class*='table-striped']")
         self.item_cart_remove_button = page.locator("[class*='btn-danger']")
+        self.wishlist_button = page.locator("#wishlist-total")
 
-    def currency_option(self, code: str):
+    def open_main_page(self) -> None:
+        self.page.goto(self.url)
+
+    def currency_option(self, code: str) -> Locator:
         return self.page.locator(f".dropdown-menu .dropdown-item[href='{code}']")
 
-    def click_add_to_cart_by_index(self, index: int = 0):
+    def click_add_to_cart_by_index(self, index: int = 0) -> None:
         button = self.add_to_cart_button.nth(index)
         button.wait_for()
         button.scroll_into_view_if_needed()
         button.click(force=True)
+
+    def add_product_to_wishlist(self, product_name: str) -> None:
+        wishlist_button = self.page.locator(
+            f'.product-thumb:has-text("{product_name}") '
+            '[formaction*="route=account/wishlist.add"]'
+        ).first
+        wishlist_button.wait_for(state="visible", timeout=5000)
+        wishlist_button.scroll_into_view_if_needed()
+        wishlist_button.click()
 
 
 
