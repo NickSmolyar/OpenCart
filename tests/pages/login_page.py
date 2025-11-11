405cbedb11b2caae9c14c7c12494c8ee46a644a8
nsmolyar <nsmolyar@tradingview.com>
2025-11-11 16:45:07 +0400

    refactored LoginPage class

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -3,41 +3,37 @@ from typing import Optional
 import pytest
 from playwright.sync_api import BrowserContext, Page
 
-from config.urls import LOGIN_URL
-from tests.pages.base_page import BaseComponent
+from config.urls import URLs
+from tests.pages.base_component import BaseComponent
 
 
 class LoginPage(BaseComponent):
     def __init__(
             self,
-            context: 'BrowserContext',
-            page: 'Page',
-            rel_url: str = '',
-            full_url: str = LOGIN_URL,
-            url_parameters: Optional[str] = None,
-            request: Optional['pytest.FixtureRequest'] = None
+            context: BrowserContext,
+            page: Page,
+            request: Optional[pytest.FixtureRequest] = None
     ):
         super().__init__(context, page, request)
-        self.rel_url = rel_url
-        self._full_url = full_url or ""
-        self._url_parameters = url_parameters
+        self.url = URLs.LOGIN
 
         self.continue_button = page.locator("a.btn-primary:has-text('Continue')")
-        self.email_input = "#input-email"
-        self.password_input = "#input-password"
-        self.login_button = page.locator("button.btn.btn-primary", has_text="Login")
-        self.logout_button = page.locator(".dropdown-menu .dropdown-item", has_text="Logout")
+        self.email_input = page.locator("#input-email")
+        self.password_input = page.locator("#input-password")
+        self.login_button = page.locator("button.btn.btn-primary:has-text('Login')")
+        self.logout_button = page.locator(".dropdown-menu .dropdown-item:has-text('Logout')")
+        self.product_search_field = page.locator("input[placeholder='Search']")
 
-    def navigate_to_login(self) -> None:
-         self.page.goto(self._full_url or self.rel_url)
+    def open_login_page(self) -> None:
+        self.page.goto(self.url)
 
     def fill_form(self, email: str, password: str) -> None:
-        self.page.fill(self.email_input, email)
-        self.page.fill(self.password_input, password)
+        self.email_input.fill(email)
+        self.password_input.fill(password)
 
-    def submit_login(self):
+    def submit_login(self) -> None:
         self.login_button.click()
 
-
-
-
+    def type_and_search(self, query: str) -> None:
+        self.product_search_field.fill(query)
+        self.product_search_field.press("Enter")
