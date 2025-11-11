ba8b1c043a19d367dca37f5bd78444c69b37b55f
nsmolyar <nsmolyar@tradingview.com>
2025-11-11 16:50:47 +0400

    refactored ContactFormPage class

--- tests/pages/contact_form_page.py
+++ tests/pages/contact_form_page.py
@@ -7,11 +7,6 @@ from tests.pages.base_component import BaseComponent
 
 
 class ContactFormPage(BaseComponent):
-    NAME_INPUT = "#input-name"
-    EMAIL_INPUT = "#input-email"
-    ENQUIRY_TEXTAREA = "#input-enquiry"
-    SUBMIT_BUTTON = "button.btn-primary[type='submit']"
-
     def __init__(
             self,
             context: BrowserContext,
@@ -21,31 +16,36 @@ class ContactFormPage(BaseComponent):
         super().__init__(context, page, request)
         self.url = URLs.CONTACT_FORM
 
+        self.name_input = page.locator("#input-name")
+        self.email_input = page.locator("#input-email")
+        self.enquiry_textarea = page.locator("#input-enquiry")
+        self.submit_button = page.locator("button.btn-primary[type='submit']")
+
     def open_contact_form(self) -> None:
         self.page.goto(self.url)
 
     def fill_contact_form(self, name: str, email: str, enquiry: str) -> None:
-        self.page.fill(self.NAME_INPUT, name)
-        self.page.fill(self.EMAIL_INPUT, email)
-        self.page.fill(self.ENQUIRY_TEXTAREA, enquiry)
+        self.name_input.fill(name)
+        self.email_input.fill(email)
+        self.enquiry_textarea.fill(enquiry)
 
     def click_submit(self) -> None:
-        self.page.click(self.SUBMIT_BUTTON)
+        self.submit_button.click()
 
     def fill_name(self, name: str) -> None:
-        self.page.fill(self.NAME_INPUT, name)
+        self.name_input.fill(name)
 
     def fill_email(self, email: str) -> None:
-        self.page.fill(self.EMAIL_INPUT, email)
+        self.email_input.fill(email)
 
     def fill_enquiry(self, enquiry: str) -> None:
-        self.page.fill(self.ENQUIRY_TEXTAREA, enquiry)
+        self.enquiry_textarea.fill(enquiry)
 
     def get_name_value(self) -> str:
-        return self.page.input_value(self.NAME_INPUT)
+        return self.name_input.input_value()
 
     def get_email_value(self) -> str:
-        return self.page.input_value(self.EMAIL_INPUT)
+        return self.email_input.input_value()
 
     def get_enquiry_value(self) -> str:
-        return self.page.input_value(self.ENQUIRY_TEXTAREA)
+        return self.enquiry_textarea.input_value()
